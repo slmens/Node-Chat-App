2ccd5dab22eb627f5ae8374e36babf3800bd129a
slmens <selimenes527@outlook.com>
2024-05-02 22:31:34 +0300

    test8

--- backend/app/server.js
+++ backend/app/server.js
@@ -22,7 +22,7 @@ app.use(express.json()); // To parse JSON bodies
 //app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://node-chat-app-flame.vercel.app/", // Replace with your client's origin
+    origin: "https://node-chat-app-flame.vercel.app", // Replace with your client's origin
     methods: ["GET", "POST", "DELETE"],
   })
 );
