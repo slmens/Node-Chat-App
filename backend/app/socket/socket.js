2859bdafd5d34826d9faec8431d292b7ed446b87
slmens <selimenes527@outlook.com>
2024-05-02 22:27:54 +0300

    test8

--- backend/app/socket/socket.js
+++ backend/app/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_ORIGIN,
+    origin: "https://node-chat-app-flame.vercel.app/",
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   },
