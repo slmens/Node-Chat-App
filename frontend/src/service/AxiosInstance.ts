94ea142d8a8abb9bd51eaa8c3ed2f787382a70fe
slmens <selimenes527@outlook.com>
2024-05-02 18:47:21 +0300

    frontend env

--- frontend/src/service/AxiosInstance.ts
+++ frontend/src/service/AxiosInstance.ts
@@ -1,13 +1,25 @@
 import axios from "axios";
 
+let API_URL;
+
+const getServerSideProps = async () => {
+  API_URL =
+    process.env.NODE_ENV === "production"
+      ? process.env.BACKEND_URL
+      : process.env.LOCAL_BACKEND_URL;
+
+  return {
+    props: {
+      data: "data",
+    },
+  };
+};
+
 const token =
   typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
 const AxiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV == "production"
-      ? "https://node-chat-app-3cox.onrender.com"
-      : "http://localhost:5000",
+  baseURL: API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
