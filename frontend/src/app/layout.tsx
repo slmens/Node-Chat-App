41ea0eb054de26294e96ff958647d29e791d557d
slmens <selimenes527@outlook.com>
2024-05-02 20:54:26 +0300

    test4

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { SocketContextProvider } from "@/context/SocketContext";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chat App",
@@ -22,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <SocketContextProvider>{children}</SocketContextProvider>
       </body>
     </html>
